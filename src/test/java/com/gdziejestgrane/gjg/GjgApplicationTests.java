ebf0a5ea2871fb888169f7a4c7eafcbe807ce41a
Darboux12 <dariuszbula98@gmail.com>
2020-08-01 16:45:44 +0200

    Backend Initial Commit

---
+++ src/test/java/com/gdziejestgrane/gjg/GjgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdziejestgrane.gjg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
